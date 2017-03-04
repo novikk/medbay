eb1f061f8e5e96033a4e09e13013b7bc7035f91e
Ivan de la Rubia <ivandelarubia@gmail.com>
2017-03-04 12:19:15 +0100

    port env var

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	_ "github.com/novikk/medbay/routers"
+	"os"
+	"strconv"
+
 	"github.com/astaxie/beego"
+	_ "github.com/novikk/medbay/routers"
 )
 
 func main() {
+	beego.BConfig.Listen.HTTPPort, _ = strconv.Atoi(os.Getenv("PORT"))
 	beego.Run()
 }
-
